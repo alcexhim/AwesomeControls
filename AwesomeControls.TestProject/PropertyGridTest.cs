2e4171b163aad4ffd940bd7ba3914c30123c8099
alcexhim <alcexhim@gmail.com>
2014-07-19 19:36:45 -0400

    Added some test cases to support some new features

--- AwesomeControls.TestProject/PropertyGridTest.cs
+++ AwesomeControls.TestProject/PropertyGridTest.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using AwesomeControls.PropertyGrid;
+
 namespace AwesomeControls.TestProject
 {
 	public partial class PropertyGridTest : Form
@@ -16,40 +18,58 @@ namespace AwesomeControls.TestProject
 		{
 			InitializeComponent();
 
+			PropertyDataType BuildActionDataType = new PropertyDataType("BuildAction",
+			new object[] 
+			{
+				"Compile", "Content", "Embedded Resource", "Code Analysis Dictionary", "Application Definition", "Page", "Resource", "Splash Screen", "Design Data", "Design Data With Design Time Creatable Types", "Entity Deploy", "Fakes"
+			}, true);
+
+			PropertyDataType CostumesDataType = new PropertyDataType("Costumes",
+			new object[] 
+			{
+				"Default", "Magical Mirai 2013", "Wonder*Girl", "Future Rider"
+			}, true);
+
 			{
 				PropertyGrid.PropertyGroup grp = new PropertyGrid.PropertyGroup("Hatsune Miku", "Character");
-				grp.Properties.Add("ClassName", "Character", true);
-				grp.Properties.Add("Name", "Hatsune Miku", false);
-				grp.Properties.Add("Costumes", "", false);
-				grp.Properties.Add("IsAvailable", true, false);
-				grp.Properties[grp.Properties.Count - 1].DataType = PropertyGrid.PropertyDataType.Boolean;
+				grp.Properties.Add(new Property("ClassName", "Character", null, true));
+				grp.Properties.Add(new Property("Name", "Hatsune Miku", null, false));
+				grp.Properties.Add(new Property("Build Action", "Compile", null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = BuildActionDataType;
+				grp.Properties.Add(new Property("Costumes", "", null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = CostumesDataType;
+				grp.Properties.Add(new Property("IsAvailable", true, null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = PropertyDataTypes.Boolean;
 				pg.Groups.Add(grp);
 			}
 			{
 				PropertyGrid.PropertyGroup grp = new PropertyGrid.PropertyGroup("Kagamine Rin", "Character");
-				grp.Properties.Add("ClassName", "Character", true);
-				grp.Properties.Add("Name", "Kagamine Rin", false);
-				grp.Properties.Add("Costumes", "", false);
-				grp.Properties.Add("IsAvailable", true, false);
-				grp.Properties[grp.Properties.Count - 1].DataType = PropertyGrid.PropertyDataType.Boolean;
+				grp.Properties.Add(new Property("ClassName", "Character", null, true));
+				grp.Properties.Add(new Property("Name", "Kagamine Rin", null, false));
+				grp.Properties.Add(new Property("Costumes", "", null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = CostumesDataType;
+				grp.Properties.Add(new Property("IsAvailable", true, null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = PropertyDataTypes.Boolean;
 				pg.Groups.Add(grp);
 			}
 			{
 				PropertyGrid.PropertyGroup grp = new PropertyGrid.PropertyGroup("Kagamine Len", "Character");
-				grp.Properties.Add("ClassName", "Character", true);
-				grp.Properties.Add("Name", "Kagamine Len", false);
-				grp.Properties.Add("Costumes", "", false);
-				grp.Properties.Add("IsAvailable", true, false);
-				grp.Properties[grp.Properties.Count - 1].DataType = PropertyGrid.PropertyDataType.Boolean;
+				grp.Properties.Add(new Property("ClassName", "Character", null, true));
+				grp.Properties.Add(new Property("Name", "Kagamine Len", null, false));
+				grp.Properties.Add(new Property("Costumes", "", null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = CostumesDataType;
+				grp.Properties.Add(new Property("IsAvailable", true, null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = PropertyDataTypes.Boolean;
 				pg.Groups.Add(grp);
 			}
 			{
 				PropertyGrid.PropertyGroup grp = new PropertyGrid.PropertyGroup("Megurine Luka", "Character");
-				grp.Properties.Add("ClassName", "Character", true);
-				grp.Properties.Add("Name", "Megurine Luka", false);
-				grp.Properties.Add("Costumes", "", false);
-				grp.Properties.Add("IsAvailable", true, false);
-				grp.Properties[grp.Properties.Count - 1].DataType = PropertyGrid.PropertyDataType.Boolean;
+				grp.Properties.Add(new Property("ClassName", "Character", null, true));
+				grp.Properties.Add(new Property("Name", "Megurine Luka", null, false));
+				grp.Properties.Add(new Property("Costumes", "", null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = CostumesDataType;
+				grp.Properties.Add(new Property("IsAvailable", true, null, false));
+				grp.Properties[grp.Properties.Count - 1].DataType = PropertyDataTypes.Boolean;
 				pg.Groups.Add(grp);
 			}
 		}
