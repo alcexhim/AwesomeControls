7daed4fadd0689573147005f402a1c1149f91875
alcexhim <alcexhim@gmail.com>
2014-09-24 15:56:48 -0400

    DropDownWindow should be borderless, border is drawn by theme

--- AwesomeControls/DropDown/DropDownWindow.Designer.cs
+++ AwesomeControls/DropDown/DropDownWindow.Designer.cs
@@ -35,7 +35,6 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(314, 127);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
 			this.Name = "DropDownWindow";
 			this.ShowInTaskbar = false;
 			this.ResumeLayout(false);
