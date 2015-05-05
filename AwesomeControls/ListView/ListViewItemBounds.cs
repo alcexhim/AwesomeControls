f3e07c41b370423f3adf98554f8e0afa364341c9
alcexhim <alcexhim@gmail.com>
2015-05-05 13:23:25 -0400

    Tabify

--- AwesomeControls/ListView/ListViewItemBounds.cs
+++ AwesomeControls/ListView/ListViewItemBounds.cs
@@ -6,116 +6,116 @@ using System.Text;
 
 namespace AwesomeControls.ListView
 {
-    public class ListViewItemBoundsCollection
-    {
-        private List<ListViewItem> itemsByIndex = new List<ListViewItem>();
-        private List<Rectangle> boundsByIndex = new List<Rectangle>();
+	public class ListViewItemBoundsCollection
+	{
+		private List<ListViewItem> itemsByIndex = new List<ListViewItem>();
+		private List<Rectangle> boundsByIndex = new List<Rectangle>();
 
-        private Dictionary<ListViewItem, Rectangle> boundsByItem = new Dictionary<ListViewItem, Rectangle>();
+		private Dictionary<ListViewItem, Rectangle> boundsByItem = new Dictionary<ListViewItem, Rectangle>();
 
-        public void Add(ListViewItem item, Rectangle bounds)
-        {
-            boundsByItem.Add(item, bounds);
+		public void Add(ListViewItem item, Rectangle bounds)
+		{
+			boundsByItem.Add(item, bounds);
 
-            itemsByIndex.Add(item);
-            boundsByIndex.Add(bounds);
-        }
+			itemsByIndex.Add(item);
+			boundsByIndex.Add(bounds);
+		}
 
-        public Rectangle GetBounds(ListViewItem item)
-        {
-            if (!boundsByItem.ContainsKey(item))
-            {
-                return Rectangle.Empty;
-            }
-            return boundsByItem[item];
-        }
+		public Rectangle GetBounds(ListViewItem item)
+		{
+			if (!boundsByItem.ContainsKey(item))
+			{
+				return Rectangle.Empty;
+			}
+			return boundsByItem[item];
+		}
 
-        public ListViewItem[] GetIntersectedItems(Rectangle rect, bool wasControlKeyPressed, int start = 0)
-        {
-            if (boundsByIndex.Count == 0) return new ListViewItem[0];
+		public ListViewItem[] GetIntersectedItems(Rectangle rect, bool wasControlKeyPressed, int start = 0)
+		{
+			if (boundsByIndex.Count == 0) return new ListViewItem[0];
 
-            int counterF = start;
-            int counterB = Count - 1;
+			int counterF = start;
+			int counterB = Count - 1;
 
 			if (counterF < 0) counterF = 0;
 
-            List<ListViewItem> lst = new List<ListViewItem>();
+			List<ListViewItem> lst = new List<ListViewItem>();
 
-            Rectangle rectF, rectB;
-            while (counterF != counterB)
-            {
-                rectF = boundsByIndex[counterF];
-                rectB = boundsByIndex[counterB];
+			Rectangle rectF, rectB;
+			while (counterF != counterB)
+			{
+				rectF = boundsByIndex[counterF];
+				rectB = boundsByIndex[counterB];
 
-                if (rectF.IntersectsWith(rect)) lst.Add(itemsByIndex[counterF]);
-                if (rectB.IntersectsWith(rect)) lst.Add(itemsByIndex[counterB]);
+				if (rectF.IntersectsWith(rect)) lst.Add(itemsByIndex[counterF]);
+				if (rectB.IntersectsWith(rect)) lst.Add(itemsByIndex[counterB]);
 
-                // if (!wasControlKeyPressed) continue;
+				// if (!wasControlKeyPressed) continue;
 
-                counterB--;
-                counterF++;
+				counterB--;
+				counterF++;
 
-                if (counterF >= Count) return lst.ToArray();
-                if (counterB < 0) return lst.ToArray();
-            }
+				if (counterF >= Count) return lst.ToArray();
+				if (counterB < 0) return lst.ToArray();
+			}
 
-            rectF = boundsByIndex[counterF];
-            rectB = boundsByIndex[counterB];
+			rectF = boundsByIndex[counterF];
+			rectB = boundsByIndex[counterB];
 
-            if (rectF.IntersectsWith(rect)) lst.Add(itemsByIndex[counterF]);
-            if (rectB.IntersectsWith(rect)) lst.Add(itemsByIndex[counterB]);
+			if (rectF.IntersectsWith(rect)) lst.Add(itemsByIndex[counterF]);
+			if (rectB.IntersectsWith(rect)) lst.Add(itemsByIndex[counterB]);
 
-            // if (!wasControlKeyPressed) continue;
+			// if (!wasControlKeyPressed) continue;
 
-            return lst.ToArray();
-        }
+			return lst.ToArray();
+		}
 
-        public bool Contains(ListViewItem lvi)
-        {
-            return boundsByItem.ContainsKey(lvi);
-        }
+		public bool Contains(ListViewItem lvi)
+		{
+			return boundsByItem.ContainsKey(lvi);
+		}
 
-        public ListViewItem GetItemAtPoint(Point point, int start = 0)
-        {
-            int counterF = start;
-            int counterB = Count - 1;
+		public ListViewItem GetItemAtPoint(Point point, int start = 0)
+		{
+			int counterF = start;
+			int counterB = Count - 1;
 
-            if (counterF >= Count) return null;
-            if (counterB < 0) return null;
+			if (counterF >= Count) return null;
+			if (counterB < 0) return null;
 
-            if (counterF < 0) counterF = 0;
+			if (counterF < 0) counterF = 0;
 
-            while (counterF != counterB)
-            {
-                Rectangle rectF = boundsByIndex[counterF];
-                if (rectF.Contains(point)) return itemsByIndex[counterF];
+			while (counterF != counterB)
+			{
+				Rectangle rectF = boundsByIndex[counterF];
+				if (rectF.Contains(point)) return itemsByIndex[counterF];
 
-                Rectangle rectB = boundsByIndex[counterB];
-                if (rectB.Contains(point)) return itemsByIndex[counterB];
+				Rectangle rectB = boundsByIndex[counterB];
+				if (rectB.Contains(point)) return itemsByIndex[counterB];
 
-                counterF++;
-                counterB--;
+				counterF++;
+				counterB--;
 
-                if (counterF >= Count) break;
-                if (counterB < 0) break;
-            }
+				if (counterF >= Count) break;
+				if (counterB < 0) break;
+			}
 
-            if (counterF >= this.Count) return null;
-            if (counterB < 0) return null;
+			if (counterF >= this.Count) return null;
+			if (counterB < 0) return null;
 
-            Rectangle rectC = boundsByIndex[counterB];
-            if (rectC.Contains(point)) return itemsByIndex[counterB];
+			Rectangle rectC = boundsByIndex[counterB];
+			if (rectC.Contains(point)) return itemsByIndex[counterB];
 
-            return null;
-        }
+			return null;
+		}
 
-        public int Count { get { return itemsByIndex.Count; } }
+		public int Count { get { return itemsByIndex.Count; } }
 
-        public void Clear()
-        {
-            itemsByIndex.Clear();
-            boundsByIndex.Clear();
-            boundsByItem.Clear();
-        }
-    }
+		public void Clear()
+		{
+			itemsByIndex.Clear();
+			boundsByIndex.Clear();
+			boundsByItem.Clear();
+		}
+	}
 }
