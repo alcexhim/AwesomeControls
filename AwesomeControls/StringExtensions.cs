b5d195ab824ba1b54e6e6a1c044cae58abed384a
alcexhim <alcexhim@gmail.com>
2015-05-18 15:44:32 -0400

    Added Capitalize() extension to System.String

---
+++ AwesomeControls/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AwesomeControls
+{
+	internal static class StringExtensions
+	{
+		public static string Capitalize(this string value)
+		{
+			if (String.IsNullOrEmpty(value)) return value;
+			if (value.Length == 1) return value.ToUpper();
+			return value[0].ToString().ToUpper() + value.Substring(1);
+		}
+	}
+}
